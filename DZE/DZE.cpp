8c61145388514009a938a1cb96e4831f62658d69
ADMIN <ADMIN@DESKTOP-CBRV7QC>
2024-03-04 16:19:14 +0500

    Добавьте файлы проекта.

---
+++ DZE/DZE.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <Windows.h>
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    int chislo;
+    std::cout << "Введите шестизначное число: \n";
+    std::cin >> chislo;
+
+    
+    
+    
+    
+    
+    return 0;
+}
